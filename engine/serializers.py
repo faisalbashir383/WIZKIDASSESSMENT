61a370904e2f0ee2d7f6d3d9e24eb045b89739ec
Faisal Ahmad <51709594+faisalbashir383@users.noreply.github.com>
2023-01-06 11:44:51 +0530

    Update serializers.py

--- engine/serializers.py
+++ engine/serializers.py
@@ -4,6 +4,19 @@ from engine.models import Recipe
 
 
 class RecipeAddViewSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(required=True)
+    category = serializers.CharField(required=True)
+    sub_category = serializers.CharField(required=True)
+    description = serializers.CharField(required=True)
+
+    class Meta:
+        model = Recipe
+        exclude = (
+            "user",
+        )
+
+
+class RecipeUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
